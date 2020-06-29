f7fab191d43fca92f9f8d715fa67bbc9a10b6a07
Jessica-Thomas <jessica.thomas648@gmail.com>
2020-06-28 22:50:35 -0400

    tried js. failed

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Carousel
+var slide = [
+    "./images/63-days.png", 
+    "./images/coral-flower-cake.jpg", 
+    "./images/meringue.jpg",
+    "./images/lemon-tart.png"];
+var i;
+
+
+function slide(){
+    document.slideshow.src = slide[i];
+}
+    if (i=0, i>slide.length, i+=1){ 
+
+        else (
+           i=0 ) 
+        
+        )
+
+function auto(){
+    window.setInterval(slide, 2000);
+}
+
+window.onload = auto();
+
+
+
+// Form Validation
+
