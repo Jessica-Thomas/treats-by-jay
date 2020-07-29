ce67950636bac3b53b8ab8bebeb2d0560a96b9a4
Jessica-Thomas <jessica.thomas648@gmail.com>
2020-07-29 18:02:26 -0400

    form does not refresh BUT variable compounds messages and does not allow submit... need to fix that

--- js/form.js
+++ js/form.js
@@ -1,38 +1,42 @@
 // FORM VALIDATION
-    // If specified fields are empty, keep track of errors and append all to one alert-- alert will no longer be blank because the messages were combined, so alert will pop up with all messages and outline errored fields in red.
+    // If specified fields are empty, keep track of errors and combine all to one alert-- alert will no longer be blank because the messages were strung together, so alert will pop up with all messages and outline errored fields in red.
     // If no errors, then form submits and alert pops up stating someone will be in contact soon. 
 
 var errorMessage = "";
 
 function checkforBlankFields() {
     
-    if (document.getElementsById('name').value == "") {
-        append`${errorMessage}Name is missing`;
-        document.getElementsById('name').style.borderColor = "red";
+    if (document.getElementById("name").value == "") {
+        errorMessage += 'HALT! Who goes here? Your name is missing. \n';
+        document.getElementById("name").style.border = "2px solid #7f36d5";
         }
 
-    if (document.getElementsById('phone').value == "") {
-        append`${errorMessage}Phone number is missing`;
-        document.getElementsById('phone').style.borderColor = "red";
+    if (document.getElementById("phone").value == "") {
+        errorMessage += 'Talking on the phone sucks, I know. But your phone number is missing. \n';
+        document.getElementById("phone").style.border = "2px solid #7f36d5";
         }
           
-    if (document.getElementsById('email').value == "") {
-        append`${errorMessage}Email address is missing`;
-        document.getElementsById('email').style.borderColor = "red";
+    if (document.getElementById("email").value == "") {
+        errorMessage += 'Give me your email so we can avoid a weird phone call. \n';
+        document.getElementById("email").style.border = "2px solid #7f36d5";
         }
 
-    if (document.getElementsById('msg').value == "") {
-        append`${errorMessage}Message to Jay is missing`;
-        document.getElementsById('msg').style.borderColor = "red";
+    if (document.getElementById("msg").value == "") {
+        errorMessage += 'Message to Jay is missing- In order to help you, I need to know what you want. ';
+        document.getElementById("msg").style.border = "2px solid #7f36d5";
         }
 
     if (errorMessage != "") {
         alert(errorMessage);
+        event.preventDefault();
+        // stops page from refreshing
         return false  
         // return false stops form from being submitted      
-        } else {
-            alert('Thanks! Someone from Treats by Jay will reach out to you soon!')
-        }
+        
+            } else {
+                alert('Thanks! Someone from Treats by Jay will reach out to you soon!')
+                return true
+            }
 
 }
 
